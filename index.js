15fb9c4bc04d8feaf33e39f909874dc49eed3159
catherine-o <catherine.ohara@outlook.com>
2019-09-08 19:58:34 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+let superbowlWin = (records) => {
+    let winner = records.find(record => record.result === 'W')
+    if (winner){
+      return winner.year
+    }
+}
